56459c0c8c9eecfe5e39582f53c6e7c9c740caa6
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-08-30 12:17:22 +0300

    get brand of first result

--- scrape-test.py
+++ scrape-test.py
@@ -1,59 +1,67 @@
-import requests
-from bs4 import BeautifulSoup as bs
+import random
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
 
 
 URL_HOME = 'https://www.lazada.vn/'
-URL_SEARCH = 'https://www.lazada.vn/catalog/?'
-URL_SEARCH_SPECIFIC = 'https://www.lazada.vn/catalog/?q=#KEYWORD#&_keyori=ss&from=input&spm=a2o4n.searchlist.search.go.12e867042bEq61'
-
-
-def get_search_data(page):
-    soup = bs(page, 'html')
-    # 'div.lzd-nav-search form'
-
-
-def two():
-    with requests.session() as session:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'
-        }
-        home_page = session.get(URL_HOME, headers=headers)
-        f = open('out.html', 'w')
-        f.write(home_page.text)
-
-        # search_data = get_search_data(home_page)
-        # results_page = session.get(URL_SEARCH, data=search_data)
-
-
-def one():
-    with requests.session() as session:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'
-        }
-        url = URL_SEARCH_SPECIFIC.replace('#KEYWORD#', 'drone')
-        page = session.get(url, headers=headers)
-        f = open('out.html', 'w')
-        f.write(page.text)
+SEARCH_TERMS = ['ipod', 'handbag', 'Webcam logitech', 'Mp3 player']
+# URL_SEARCH = 'https://www.lazada.vn/catalog/?'
+# URL_SEARCH_SPECIFIC = 'https://www.lazada.vn/catalog/?q=#KEYWORD#&_keyori=ss&from=input&spm=a2o4n.searchlist.search.go.12e867042bEq61'
 
 
 def three():
+    search_term = random.choice(SEARCH_TERMS)
     browser = webdriver.Firefox()
     browser.get(URL_HOME)
     q_element = browser.find_element_by_id('q')
-    q_element.send_keys('drone')
+    q_element.send_keys(search_term)
     search_form = browser.find_element_by_css_selector('.lzd-nav-search > form')
-    results_page = search_form.submit()
-    html = browser.page_source
-    # soup = bs(html)
+    search_form.submit()
 
-    f = open('out.html', 'w')
-    f.write(html)
+    # wait for page to be loaded by ajax
+    delay = 10 # seconds
+    try:
+        products = WebDriverWait(browser, delay).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, 'div[data-qa-locator="product-item"]')
+            )
+        )
+        products = browser.find_elements_by_css_selector(
+            'div[data-qa-locator="product-item"]')
+        print(f"There are {len(products)} products on the page")
 
-    browser.quit()
+        # scroll to end to force-load all images, then scroll back up
+        # browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+        # time.sleep(1)
+        # browser.execute_script('window.scrollTo(0, 0)')
 
 
+        i = 1
+        links = [product.find_element_by_tag_name('a') for product in products]
+        product_urls = [link.get_attribute('href') for link in links]
+        for url in [product_urls[0]]:
+            print(f"{i}\tproduct URL: {url}")
+            # open a new tab
+            browser.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
+            browser.get(url)
+            brand_link = browser.find_element_by_class_name(
+                'pdp-product-brand__brand-link')
+            brand = brand_link.get_attribute('text')
+            print(f"Brand: {brand}")
+
+            # close the tab
+            browser.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w')
+            i += 1
+    except TimeoutException:
+        print("That took too long!")
+
+    # browser.quit()
 
 
 three()
