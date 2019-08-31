90ba21bece71fa4e8653b0a5150ce3881c2c730d
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-08-31 14:49:31 +0300

    add scraper classes

---
+++ scraper/scraper/base.py
@@ -0,0 +1,9 @@
+class Scraper:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def _get_search_results(self, search_term, results_limit):
+        pass
+
+    def get_brand_share(self, search_term, results_limit):
+        pass
---
+++ scraper/scraper/lazada.py
@@ -0,0 +1,48 @@
+from math import ceil
+from ..browser import ScraperBrowser
+from ..page.lazada.pages import HomePage, ProductPage, SearchPage
+from .base import Scraper
+
+class LazadaScraper(Scraper):
+    RESULTS_PER_PAGE = 40
+
+    def get_brand_share(self, search_term, results_limit):
+        results = []
+        amt_pages = ceil(results_limit / self.RESULTS_PER_PAGE)
+
+        home_page = HomePage(self.browser)
+        home_page.load().search(search_term)
+        self._wait_for_search_results(self.browser)
+        print("searched!")
+
+        base_url = self.browser.current_url
+        search_urls = [base_url]
+        for i in range(1, amt_pages):
+            search_urls.append(f"{base_url}&page={i + 1}")
+
+        print("search page urls:")
+        # [print(url) for url in search_urls]
+
+        eager_browser = self.browser
+        for url in search_urls:
+            eager_browser.get(url)
+            search_page = SearchPage(eager_browser)
+            page_results = search_page.get_results()
+            results += page_results
+
+        print("search result urls:")
+        print(results)
+
+        # for result in results[0:4]:
+        #     product_page = ProductPage(self.browser)
+        #     product_page.open_new_tab().load(result)
+        #     brand = product_page.get_brand()
+        #     print(f"Brand: {brand}")
+        #     product_page.close_tab()
+
+        return [1, 2, 3]
+
+    def _wait_for_search_results(self, browser):
+        search_page = SearchPage(browser)
+        search_page.wait_for_results_load()
+
