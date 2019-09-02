a7e2ce52e08dd4baf66dad789914870e2ad49301
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-09-02 17:47:26 +0300

    search/ route for json

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, current_app
+from flask import Flask, current_app, request
 from flask import jsonify
 # from scraper.browser import ScraperBrowser
 from scraper.scraper.lazada_simple import LazadaSimpleScraper
@@ -11,6 +11,15 @@ app.config.from_pyfile('app.cfg')
 @app.route('/')
 def main():
     return current_app.send_static_file('index.html')
+
+
+@app.route('/search/')
+def search():
+    search_term = request.args.get('term')
+    scraper_api_key = app.config['SCRAPERAPI_KEY']
+    scraper = LazadaSimpleScraper().set_scraper_api_key(scraper_api_key)
+    shares = scraper.get_brand_shares(search_term, 100)
+    return jsonify(shares)
 
 
 @app.route('/test/')
