5f754ebfc2adc8561f329102df09c3d9a5519037
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-09-02 11:35:26 +0300

    get from multiple pages

--- scraper/scraper/lazada_simple.py
+++ scraper/scraper/lazada_simple.py
@@ -1,9 +1,12 @@
+from math import ceil
 import random
 import re
 import requests
 from .base import SimpleScraper
 
 class LazadaSimpleScraper(SimpleScraper):
+    RESULTS_PER_PAGE = 40
+
     SEARCH_URL = "https://www.lazada.vn/catalog/?q=##SEARCH_TERM##" \
         + "&_keyori=ss&from=input&spm=a2o4n.home.search.go.1905e18214cAJy"
 
@@ -14,17 +17,37 @@ class LazadaSimpleScraper(SimpleScraper):
     ]
 
     def get_brand_share(self, search_term, results_limit):
-        url = self.SEARCH_URL.replace('##SEARCH_TERM##', search_term)
+        base_url = self.SEARCH_URL.replace('##SEARCH_TERM##', search_term)
+        amt_pages = ceil(results_limit / self.RESULTS_PER_PAGE)
+        print(f"pages to load: {amt_pages}")
+
+        brands = []
         with requests.session() as session:
             headers = {
                 'User-Agent': random.choice(self.USER_AGENTS)
             }
-            page = session.get(url, headers=headers)
-            # f = open(f"{search_term.replace(' ', '_')}-out.html", 'w')
-            # f.write(page.text)
+            # First page
+            print("loading first page")
+            print(base_url)
+            page = session.get(base_url, headers=headers)
+            brands += self.get_brands(page.text)
+            f = open(f"{search_term.replace(' ', '_')}-out.html", 'w')
+            f.write(page.text)
 
-            brands = self.get_brands(page.text)
-            return brands
+            # Additional pages
+            for i in range(1, amt_pages):
+                print(f"loading page {i + 1}")
+                url = f"{base_url}&page={i + 1}"
+                print(url)
+                page = session.get(url, headers=headers)
+
+                f = open(f"{search_term.replace(' ', '_')}-{i + 1}-out.html", 'w')
+                f.write(page.text)
+
+                page_brands = self.get_brands(page.text)
+                print(f"found {len(page_brands)} brands on page")
+
+        return brands
 
     def get_brands(self, html):
         regex = re.compile('"brandName":"([^"]+)"')
