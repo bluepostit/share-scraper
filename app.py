440fe427b5f06ba883c212d7162d9e1e3a39f8c9
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-09-03 11:15:53 +0300

    cleanup

--- app.py
+++ app.py
@@ -36,28 +36,3 @@ def search():
         ]
     }
     return jsonify(shares)
-
-
-@app.route('/test/')
-def test_scraper():
-    scraper_api_key = app.config['SCRAPERAPI_KEY']
-    search_terms = ['ipod', 'handbag', 'Webcam logitech', 'Mp3 player']
-    search_term = random.choice(search_terms)
-    scraper = LazadaSimpleScraper().set_scraper_api_key(scraper_api_key)
-    shares = scraper.get_brand_shares(search_term, 100)
-    return jsonify(shares)
-
-
-@app.route('/collect/')
-def test_collect():
-    f = open('mp3-player-brands-simple.txt')
-    brands = f.readlines()
-    print(f"total brands: {len(brands)}")
-    shares = {}
-    for brand in brands:
-        if brand in shares:
-            shares[brand] += 1
-        else:
-            shares[brand] = 1
-    print(shares)
-    return jsonify(shares)
